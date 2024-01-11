4564e1445046d0cd6b4276ca51b46533b9108d84
tccsantos <cortez.thiago@outlook.com>
2024-01-10 21:12:09 -0300

    Retweets update
    Now it works better and with GML

--- Retweets.py
+++ Retweets.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import csv
+import codecs
 import re
 import argparse
 import sys
+from collections import Counter
 
 from argparse import RawTextHelpFormatter
+
 
 def read_options():
     status = False
@@ -30,27 +33,70 @@ def read_options():
 
     return {"success": status, "input": argument.input, "output": argument.output}
 
+def contagem(lista):
+    c = Counter(lista)
+    return c.most_common()
 
-def contagem(input, output):
+def analise(input):
     df = pd.read_csv(input)
-    limpagem = re.compile("\W")
+    limpagem = re.compile(r"[\W]")
     rta = re.compile('RT @')
+    resultado = []
     for ind in df.index:
-        with open(output + '_rt.csv', 'a', newline='') as arquivo:
-            writer = csv.writer(arquivo)
-            try:
-                if rta.search(str(df['text'][ind])):
-                    array = rta.split(str(df['text'][ind]))
-                    for i in range(1, len(array)):
-                        word_token = array[i].split()
-                        word_token = limpagem.sub("", word_token[0])
-                        writer.writerow((df["username"][ind],word_token))
-            except Exception as e:
-                print(e)
-                break
+        try:
+            if rta.search(str(df['text'][ind])):
+                array = rta.split(str(df['text'][ind]))
+                names = []
+                for i in range(1, len(array)):
+                    word_token = array[i].split()
+                    word_token = limpagem.sub("", word_token[0])
+                    word_token = word_token.lower()
+                    if array[i - 1] != '':
+                        if not limpagem.search(str(array[i - 1][-1])):
+                            continue
+                    names.append(word_token)
+                    if i == 1: resultado.append((str(df["username"][ind]).lower(), names[0]))
+                    else: resultado.append((names[i - 2], names[i - 1]))
+        except Exception as e:
+            print(e)
+            print(ind)
+            continue
+    return resultado
+
+def contnoh(lista):
+    #set()
+    apoio1 = []
+    apoio2 = []
+    for ind in range(len(lista)):
+        apoio1.append(lista[ind][0][0])
+        apoio2.append(lista[ind][0][1])
+    apoio = apoio1 + apoio2
+    c = Counter(apoio)
+    sup = c.most_common()
+    final = []
+    for i in range(len(sup)):
+        final.append(sup[i][0])
+    return final
+
+def escrita(output, aresta):
+    noh = contnoh(aresta)
+    with codecs.open(output + "_retweets.gml", 'w', encoding='utf-8') as arquivo:
+        arquivo.write("graph\n[\n")
+        for ind in range(len(noh)):
+            node = noh[ind]
+            arquivo.write(f'\tnode\n\t[\n\t\tid {str(node)}\n\t\tlabel "{str(node)}"\n\t]\n')
+        for i in range(len(aresta)):
+            source = aresta[i][0][0]
+            target = aresta[i][0][1]
+            weight = aresta[i][1]
+            label = f'aresta {str(source)} para {str(target)}'
+            arquivo.write(f'\tedge\n\t[\n\t\tsource {str(source)}\n\t\ttarget {str(target)}\n\t\tlabel "{str(label)}"\n\t\tweight {str(weight)}\n\t]\n')
+        arquivo.write("]")
 
 #Somente CSV por enquanto
 result = read_options()
 if not result.get("success"):
     sys.exit(1)
-contagem(result.get("input"), result.get("output"))
+resultado = analise(result.get("input"))
+processo = contagem(resultado)
+escrita(result.get("output"), processo)
